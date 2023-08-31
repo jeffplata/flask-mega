7a1934ad2107c94cad28efa348327befcfe905a4
jeffplata <jeffplata@yahoo.com>
2023-08-31 20:09:38 +0800

    chapter 4 - database

--- microblog.py
+++ microblog.py
@@ -1,1 +1,10 @@
+# most basic
 from app import app
+
+# for the shell context processor
+from app import db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
