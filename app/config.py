ddb346bb7c8e62901797343add95d43196b0cef7
jeffplata <jeffplata@yahoo.com>
2023-09-30 14:29:39 +0800

    config DATABASE_UTL to DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-random-text'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_UTL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
